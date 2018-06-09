c6cf24cb7e96e1cdc90e5f34ec311dceb369485c
supertiger1234 <saawalkhn@googlemail.com>
2018-06-09 18:33:12 +0100

    Finished setup page, working on main os.

--- index.js
+++ index.js
@@ -2,15 +2,58 @@ const electron = require('electron');
 const {
     app,
     BrowserWindow
-} = electron;
+} = require('electron');
+const {
+    ipcMain
+} = require('electron');
+
 const fs = require('fs');
+var database = {};
+
+
+function readFile(callback){
+    fs.readFile('db.json', 'utf8', function readFileCallback(err, data) {
+        if (err) {
+            callback(err)
+        } else {
+            database = JSON.parse(data);
+            callback(true)
+        }
+    });
+    
+}
 
 
 app.on('ready', () => {
-    let win = new BrowserWindow({
-        fullscreen: true,
-        frame: false,
-        backgroundColor: "#000000"
-    });
-    win.loadURL(`file://${__dirname}/FirstBoot/index.html`)
+    readFile(function(cb){
+        let win = new BrowserWindow({
+            fullscreen: true,
+            frame: false,
+            backgroundColor: "#000000"
+        });
+        if (typeof database.accounts == "undefined"){
+            win.loadURL(`file://${__dirname}/FirstBoot/index.html`)
+        }else{
+            win.loadURL(`file://${__dirname}/SystemUI/index.html`)
+        }
+
+    })
+    ipcMain.on('setupComplete', (event, detailsConfig) => {
+        database["accounts"] = {detailsConfig}
+        saveDatabase()
+        console.log(database)
+
+    })
 });
+
+
+function saveDatabase() {
+
+    var json = JSON.stringify(database)
+    fs.writeFile('db.json', json, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+
+}
